ce6845c3623f1b301bc173a5a2c849ef591900fe
Hizrok <jan.kapsa@seznam.cz>
2023-10-24 23:53:55 +0200

    feat: simple api for users

---
+++ server/routes/users/routes.js
@@ -0,0 +1,16 @@
+const { Router } = require("express");
+
+const controller = require("./controller");
+
+const router = Router();
+
+router.get("/", controller.get_users); 
+router.get("/:login", controller.get_user_by_login);
+
+router.post("/", controller.add_user);
+
+router.put("/:login", controller.edit_user);
+
+router.delete("/:login", controller.delete_user);
+
+module.exports = router;
