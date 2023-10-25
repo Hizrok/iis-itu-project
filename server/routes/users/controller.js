0a119458e56129873283531c1cc74a68ce77ed29
Hizrok <jan.kapsa@seznam.cz>
2023-10-25 22:37:31 +0200

    change: changed controller functions to use async / await

--- server/routes/users/controller.js
+++ server/routes/users/controller.js
@@ -2,84 +2,95 @@
 const pool = require("../../db");
 const queries = require("./queries");
 
-const handle_error = (res, error) => {
-	res.status(500).json({error});
-	console.error(error);
-}
-
-const get_users = (req, res) => {
-	pool.query(queries.get_all_users, (error, results) => {
-		if (error) { handle_error(res, error); return; }
-		res.status(200).json(results.rows);
-	});
+const get_users = async (req, res) => {
+	try {
+		const users = await pool.query(queries.get_all_users);
+		res.status(200).json(users.rows);
+	} catch (error) {
+		res.status(500).json(error);
+		console.error(error);
+	}
 };
 
-const get_user_by_login = (req, res) => {
-	const login = req.params.login;
-	pool.query(queries.get_user_by_login, [login], (error, results) => {
-		if (error) { handle_error(res, error); return; }
-
-		if (results.rows.length) {
-			res.status(200).json(results.rows);
+const get_user_by_login = async (req, res) => {
+	try {
+		const login = req.params.login;
+		const user = await pool.query(queries.get_user_by_login, [login]);
+		if (user.rows.length) {
+			res.status(200).json(user.rows);
 		} else {
-			res.status(404).json({error: "user not found"});
+			res.status(404).json({error: `user ${login} not found`});
 		}
-	});
+	} catch (error) {
+		res.status(500).json(error);
+		console.error(error);
+	}
 };
 
-const add_user = (req, res) => {
-	
-	const { role, name, surname } = req.body;
+const generate_login = async surname_substring => {
+	let login = surname_substring;
+	const similar_logins = await pool.query(queries.get_similar_logins, [login]);
 
-	let login = surname.substring(0, 5).toLowerCase();
+	taken_numbers = similar_logins.rows.map(l => l.user_login);
+	taken_numbers = taken_numbers.filter(l => login.length === l.substring(0,l.length-2).length)
+	taken_numbers = taken_numbers.map(l => parseInt(l.substring(l.length-2, l.length)));
 
-	pool.query(queries.get_similar_logins, [login], (error, results) => {
-		if (error) { handle_error(res, error); return; }
-
-		// e.g.: login = kap, login kapsa00 in db will get returned
-		taken_numbers = results.rows.map(l => l.user_login);
-		taken_numbers = taken_numbers.filter(l => login.length === l.substring(0,l.length-2).length)
-		taken_numbers = taken_numbers.map(l => parseInt(l.substring(l.length-2, l.length)));
-
-		for (let i = 0; i < 100; i++) {
-			if (!taken_numbers.includes(i)) {
-				login_number = "";
-				if (i < 10) {
-					login_number += "0";
-				}
-				login_number += i.toString();
-				login += login_number;
-				break;
+	for (let i = 0; i < 100; i++) {
+		if (!taken_numbers.includes(i)) {
+			login_number = "";
+			if (i < 10) {
+				login_number += "0";
 			}
+			login_number += i.toString();
+			login += login_number;
+			break;
 		}
+	}
 
-		pool.query(queries.add_user, [role, login, name, surname], (error, results) => {
-			if (error) { handle_error(res, error); return; }
-			res.status(201).json({message: "successfully created new user"});
-		});
-
-	});
-};
-
-const edit_user = (req, res) => {
-	const login = req.params.login;
-	const { role, name, surname } = req.body;
-
-	pool.query(queries.edit_user, [role, name, surname, login], (error, results) => {
-		if (error) {handle_error(res, error)}
-		res.status(202).json({message: "successfully edited new user"});
-	});
-
+	return login;
 }
 
-const delete_user = (req, res) => {
-	const login = req.params.login;
+const add_user = async (req, res) => {
+	try {
+		const { role, name, surname } = req.body;
+		const surname_substring = surname.substring(0, 5).toLowerCase();
+		const login = await generate_login(surname_substring);
 
-	pool.query(queries.delete_user, [login], (error, results) => {
-		if (error) {handle_error(res, error)}
-		res.status(202).json({message: "successfully deleted new user"});
-	});
+		const user = await pool.query(queries.add_user, [role, login, name, surname]);
+		res.status(201).json(user.rows);		
+	} catch (error) {
+		res.status(500).json(error);
+		console.error(error);
+	}
+};
 
+const edit_user = async (req, res) => {
+	try {
+		const login = req.params.login;
+		const { role, name, surname } = req.body;
+	
+		const user = await pool.query(queries.edit_user, [role, name, surname, login]);
+		res.status(202).json(user.rows);
+	} catch (error) {
+		res.status(500).json(error);
+		console.error(error);
+	}
+}
+
+const delete_user = async (req, res) => {
+	try {
+		const login = req.params.login;
+		const delete_query = await pool.query(queries.delete_user, [login]);	
+		// delete query will be one if user with that login was found
+		if (delete_query.rowCount) {
+			res.status(202).json({message: `successfully deleted user ${login}`});
+		} else {
+			res.status(404).json({message: `user ${login} was not found`});
+		}
+	} catch (error) {
+		res.status(500).json(error);
+		console.error(error);	
+	}
 }
 
 module.exports = {
--- server/routes/users/queries.js
+++ server/routes/users/queries.js
@@ -1,9 +1,9 @@
 
 const get_all_users = "select * from users;";
 const get_user_by_login = "select * from users where user_login = $1";
-const add_user = "insert into users (user_role, user_login, user_name, user_surname) values ($1, $2, $3, $4);"
+const add_user = "insert into users (user_role, user_login, user_name, user_surname) values ($1, $2, $3, $4) returning *;"
 const get_similar_logins = "select user_login from users where user_login like $1 || '%';";
-const edit_user = "update users set user_role=$1, user_name=$2, user_surname=$3 where user_login=$4;";
+const edit_user = "update users set user_role=$1, user_name=$2, user_surname=$3 where user_login=$4 returning *;";
 const delete_user = "delete from users where user_login=$1;";
 
 module.exports = {
