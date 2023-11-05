af871cb9664035c717d5196788694721ac2182d2
Hizrok <jan.kapsa@seznam.cz>
2023-11-05 22:33:45 +0100

    feat: fetching users

--- client/src/pages/course/MainCoursesListPage.tsx
+++ client/src/pages/course/MainCoursesListPage.tsx
@@ -1,6 +1,40 @@
+
+import { useState, useEffect } from 'react';
+
+type User = {
+    user_login: string,
+    user_role: string,
+    user_name: string,
+    user_surname: string
+}
+
 const MainCoursesListPage = () => {
+    
+    const [users, setUsers] = useState<User[]>([]);
+
+    // let courses = "loading..."
+
+    useEffect(() => {
+        async function fetchCourses() {
+            const response = await fetch("http://localhost:3000/users");
+            const users_json = await response.json();
+    
+            setUsers(users_json);
+            console.log(users_json);
+        }
+        
+        fetchCourses();
+    }, []);
+
+
     return(
-        <div>MainCoursesListPage</div>
+        <div>
+            {
+                users.map(user => {
+                    return <div>{user.user_name}</div>
+                })
+            }
+        </div>
     )
 }
 
