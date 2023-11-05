0df5509fae79b4cf06c1de0e98b9b1b729c1c2cb
Tomáš Vlach <xvlach24@vutbr.cz>
2023-11-05 22:40:54 +0100

    Added basic fetching of courses and listing of course IDs

--- client/src/pages/list/CourseListPage.tsx
+++ client/src/pages/list/CourseListPage.tsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react";
+
 const CourseListPage = () => {
+    type Course = {
+        course_id: string,
+        course_name: string,
+        course_annotation: string,
+        course_guarantor_login: string
+    }
+        
+    const [courses, setCourses] = useState<Course[]>([]);
+
+    // let courses = "loading..."
+
+    useEffect(() => {
+        async function fetchCourses() {
+            const response = await fetch("http://localhost:3000/users");
+            const courses_json = await response.json();
+    
+            setCourses(courses_json);
+            console.log(courses_json);
+        }
+        
+        fetchCourses();
+    }, []);
+
+
     return(
-        <div>CourseListPage</div>
+        <div>
+            {
+                courses.map(course => {
+                    return <div>{course.course_id}</div>
+                })
+            }
+        </div>
     )
 }
 
