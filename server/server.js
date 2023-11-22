11b753b4f8887ec5f9d46a5f94cc2c9ccdd84a27
Hizrok <jan.kapsa@seznam.cz>
2023-11-22 22:09:26 +0100

    feat: user authorization

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const pool = require("./db");
+
+const bcrypt = require("bcrypt");
+const saltRounds = 12;
 
 const user_router = require("./routes/users/routes");
 const course_router = require("./routes/courses/routes");
@@ -14,16 +18,36 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-app.post("/login", (req, res) => {
-  // TODO: authenticate
-  const { login, role } = req.body;
-  const user = {
-    login,
-    role,
-  };
+app.post("/login", async (req, res) => {
+  try {
+    const { login, password } = req.body;
 
-  const access_token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  res.status(201).json({ token: access_token });
+    const user_query = await pool.query(
+      "select * from users where user_login = $1",
+      [login]
+    );
+
+    if (user_query.rows.length == 0) {
+      return res.status(404).json({ error: `user ${login} not found` });
+    }
+
+    const hash = user_query.rows[0].user_password;
+    bcrypt.compare(password, hash, function (err, result) {
+      if (err) throw err;
+      if (!result) return res.sendStatus(403);
+
+      const user = {
+        login,
+        role: user_query.rows[0].user_role,
+      };
+
+      const access_token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+      res.status(201).json({ token: access_token });
+    });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 });
 
 app.use("/users", user_router);
