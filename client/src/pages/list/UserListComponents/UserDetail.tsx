4214956ffd4eae90eb6ef3417beba10156358bfe
petr.teichgrab <petr.teichgrab@ais-servis.cz>
2023-11-23 15:27:33 +0100

    basic user list without editing and deleting functionality

---
+++ client/src/pages/list/UserListComponents/UserDetail.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import User from "../../../components/common/Types/User";
+import '../../../styles/style.css';
+
+interface UserDetailProps {
+    selectedUser: User;
+    onEditUser: (user: User) => void;
+}
+
+const UserDetail: React.FC<UserDetailProps> = (props) => {
+
+    const { selectedUser, onEditUser } = props;
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedName, setEditedName] = useState(selectedUser.user_name);
+  const [editedSurname, setEditedSurname] = useState(selectedUser.user_surname);
+
+  const handleEditClick = () => {
+    setEditedName(selectedUser.user_name);
+    setIsEditing(true);
+  };
+
+  const handleSaveClick = () => {
+    selectedUser.user_name = editedName;
+    onEditUser(selectedUser)
+    setIsEditing(false);
+  };
+  return (
+    <div className="user-detail-container">
+      <div className="purple-bar"></div>
+      <img src="https://www.pngkey.com/png/full/988-9886269_blank-person-facebook-no-profile.png" alt="Fotka uživatele" />
+      <div className="user-detail-properties">
+        <p>Jméno:</p>{' '}
+          {isEditing ? (
+            <input
+              type="text"
+              value={editedName}
+              onChange={(e) => setEditedName(e.target.value)}
+            />) : (<p>{selectedUser.user_name}</p>
+          )}
+        <p>Přijmení:</p>{' '}
+          {isEditing ? (
+            <input
+              type="text"
+              value={editedSurname}
+              onChange={(e) => setEditedSurname(e.target.value)}
+            />) : (<p>{selectedUser.user_surname}</p>
+          )}
+        {isEditing ? (
+          <button onClick={handleSaveClick}>Uložit</button>
+        ) : (
+          <button onClick={handleEditClick}>Upravit</button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default UserDetail;
--- client/src/pages/list/UserListPage.tsx
+++ client/src/pages/list/UserListPage.tsx
@@ -1,7 +1,82 @@
+import React, { useState, useEffect } from "react";
+import User from "../../components/common/Types/User";
+import '../../styles/style.css';
+import UserDetail from "./UserListComponents/UserDetail";
+
 const UserListPage = () => {
-    return(
-        <div>UserListPage</div>
-    )
-}
+    const [users, setUsers] = useState<User[]>([]);
+    const [selectedUser, setSelectedUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        async function fetchUsers() {
+            try {
+                const response = await fetch("http://localhost:3000/users");
+                const users_json = await response.json();
+                setUsers(users_json);
+                console.log(users_json);
+            } catch (error) {
+                console.error("Error fetching users:", error);
+            }
+        }
+
+        fetchUsers();
+    }, []);
+
+    const handleUserClick = (user: User) => {
+        setSelectedUser(user);
+    };
+
+    const handleEditUser = (editedUser: User) => {
+        console.log(editedUser);
+        
+        const toBeUpdatedUser = {
+            user_role : editedUser.user_role,
+            user_name : editedUser.user_name,
+            user_surname : editedUser.user_surname
+        };
+
+        async function fetchCourses() {
+            const request = await fetch("http://localhost:3000/users/" + editedUser.user_login, {
+              method: "PUT",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(toBeUpdatedUser),
+            });
+            const request_json = await request.json();
+            console.log(request_json);
+          }
+          fetchCourses();
+    };
+
+    const handleDeleteUser = () => {
+        console.log("Smazat uživatele:", selectedUser);
+    };
+
+    return (
+        <div className="users-page">
+            <div className="list-pages-list-container">
+                <h2>Seznam Uživatelů</h2>
+                <ul>
+                    <li className="list-header">
+                        <span className="header-item">Jméno</span>
+                        <span className="header-item">Příjmení</span>
+                        <span className="header-item">Role</span>
+                    </li>
+                    {users.map((user) => (
+                        <li key={user.user_login} className="list-item-properties" onClick={() => handleUserClick(user)}>
+                            <span className="list-item-property">{user.user_name}</span>
+                            <span className="list-item-property">{user.user_surname}</span>
+                            <span className="list-item-property user-role">{user.user_role}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            {selectedUser && (
+                <UserDetail selectedUser={selectedUser} onEditUser={handleEditUser}></UserDetail>
+            )}
+        </div>
+    );
+};
 
 export default UserListPage;
