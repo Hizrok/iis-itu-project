0c3639c79b69e9ec802f5710d4f2487d00d9ada4
Hizrok <jan.kapsa@seznam.cz>
2023-11-27 21:35:46 +0100

    fix: course registration

--- client/src/pages/registration/CourseRegPage.tsx
+++ client/src/pages/registration/CourseRegPage.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setLoadingContentState } from "../../redux/features/LoadingContentStateSlice";
 import { useAuthHeader, useAuthUser } from "react-auth-kit";
-import Course from "../../components/common/Types/Course";
+// import Course from "../../components/common/Types/Course";
 import { Box, Checkbox } from "@mui/material";
 
 type CourseRegistration = {
@@ -16,6 +16,7 @@ const CourseRegPage = () => {
   const authHeader = useAuthHeader();
   const auth = useAuthUser();
 
+  const [status, setStatus] = useState("");
   const [courses, setCourses] = useState<CourseRegistration[]>([]);
 
   useEffect(() => {
@@ -23,16 +24,8 @@ const CourseRegPage = () => {
       try {
         dispatch(setLoadingContentState(true));
 
-        const courses_response = await fetch("http://localhost:3000/courses", {
-          method: "GET",
-          headers: {
-            authorization: authHeader(),
-          },
-        });
-        const courses_json = await courses_response.json();
-
-        const registered_response = await fetch(
-          `http://localhost:3000/registrations/courses/${auth()?.id}`,
+        const status_response = await fetch(
+          `${import.meta.env.VITE_SERVER_HOST}registrations/active`,
           {
             method: "GET",
             headers: {
@@ -40,20 +33,27 @@ const CourseRegPage = () => {
             },
           }
         );
+        const status_json = await status_response.json();
+        console.log(status_json);
 
-        let registered: string[] = [];
-        if (registered_response.ok) {
-          const registered_json = await registered_response.json();
-          registered = registered_json.map((course: Course) => course.id);
+        setStatus(status_json);
+
+        if (status_json !== "IDLE") {
+          const courses_response = await fetch(
+            `${import.meta.env.VITE_SERVER_HOST}registrations/courses/${
+              auth()?.id
+            }/reg_data`,
+            {
+              method: "GET",
+              headers: {
+                authorization: authHeader(),
+              },
+            }
+          );
+          const courses_json = await courses_response.json();
+
+          setCourses(courses_json);
         }
-
-        setCourses(
-          courses_json.map((course: Course) => ({
-            id: course.id,
-            name: course.name,
-            registered: registered.includes(course.id),
-          }))
-        );
 
         dispatch(setLoadingContentState(false));
       } catch (error) {
@@ -71,22 +71,25 @@ const CourseRegPage = () => {
         dispatch(setLoadingContentState(true));
 
         if (!registered) {
-          await fetch("http://localhost:3000/registrations/courses", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              authorization: authHeader(),
-            },
-            body: JSON.stringify({
-              course: courses[index].id,
-              student: auth()?.id,
-            }),
-          });
+          await fetch(
+            `${import.meta.env.VITE_SERVER_HOST}registrations/courses`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                authorization: authHeader(),
+              },
+              body: JSON.stringify({
+                course: courses[index].id,
+                student: auth()?.id,
+              }),
+            }
+          );
         } else {
           await fetch(
-            `http://localhost:3000/registrations/courses/${courses[index].id}/${
-              auth()?.id
-            }`,
+            `${import.meta.env.VITE_SERVER_HOST}registrations/courses/${
+              courses[index].id
+            }/${auth()?.id}`,
             {
               method: "DELETE",
               headers: {
@@ -115,19 +118,24 @@ const CourseRegPage = () => {
 
   return (
     <Box>
-      {courses.map((course: CourseRegistration, i) => (
-        <Box
-          key={i}
-          sx={{ display: "grid", gridTemplateColumns: "70px 500px 50px" }}
-        >
-          <p>{course.id}</p>
-          <p>{course.name}</p>
-          <Checkbox
-            checked={course.registered}
-            onChange={() => handleChange(i)}
-          />
-        </Box>
-      ))}
+      {status !== "IDLE" ? (
+        courses.map((course: CourseRegistration, i) => (
+          <Box
+            key={i}
+            sx={{ display: "grid", gridTemplateColumns: "70px 500px 50px" }}
+          >
+            <p>{course.id}</p>
+            <p>{course.name}</p>
+            <Checkbox
+              disabled={status !== "COURSES IN PROGRESS"}
+              checked={course.registered}
+              onChange={() => handleChange(i)}
+            />
+          </Box>
+        ))
+      ) : (
+        <p>modul registrací předmětů ještě nebyl spuštěn</p>
+      )}
     </Box>
   );
 };
