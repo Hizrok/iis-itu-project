83fa417e51b3f74bed0a122e32fb4154e149d64c
Tomáš Vlach <xvlach24@vutbr.cz>
2023-12-17 00:47:17 +0100

    feat: guarantied courses

--- client/src/components/axios/CourseAxios.tsx
+++ client/src/components/axios/CourseAxios.tsx
@@ -103,3 +103,42 @@ export const editCourse = async (
       .catch(error => { console.error(error); throw error; });
   };
 
+  export const editCourseNoGuarantor = async (
+    id: string,
+    name: string,
+    annotation: string,
+    guarantor: string,
+    index: number,
+    setCourses: Function,
+    setSelected: Function,
+    authHeader: Function,
+  ) => {
+    return await axios
+      .put(
+        `${import.meta.env.VITE_SERVER_HOST}courses/${id}`,
+        {
+          id,
+          name,
+          annotation,
+        },
+        {
+          headers: {
+            Authorization: authHeader(),
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data.msg);
+        setCourses((oldCourses: Course[]) => {
+          const newCourses = [...oldCourses];
+          newCourses[index].id = id;
+          newCourses[index].name = name;
+          newCourses[index].annotation = annotation;
+          newCourses[index].guarantor = guarantor;
+          return newCourses;
+        });
+        setSelected(id);
+      })
+      .catch(error => { console.error(error); throw error; });
+  };
+
